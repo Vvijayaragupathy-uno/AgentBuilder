5b61b8e9792db2b0fa0debfd3ac8c69678d1dd73
AI-CCORE <unoaiccore@unomaha.edu>
2026-03-11 18:52:44 -0500

    d

--- langflow/src/frontend/start-nginx.sh
+++ langflow/src/frontend/start-nginx.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Set default backend URL if not provided (internal Railway hostname)
+export BACKEND_URL=${BACKEND_URL:-"http://agentbuilder.railway.internal:8080"}
+
+echo "Configuring Nginx proxy to: $BACKEND_URL"
+
 # Replace the placeholder with the actual value
-sed -i "s|__BACKEND_URL__|$BACKEND_URL|g" /etc/nginx/conf.d/default.conf
+sed -i "s|__BACKEND_URL__|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
 
 
 # Start nginx
